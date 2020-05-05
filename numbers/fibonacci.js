88e3c81d9d12ddda7538211bb34ec77386584f8a
keenoro <kkhoo93@gmail.com>
2020-05-05 15:22:02 +1000

    fibonacci problem and solution

---
+++ numbers/fibonacci.js
@@ -0,0 +1,65 @@
+// FIBONACCI SERIES
+// Print the n-th number in the fibnoacci series
+// There was 3 ways to solve it
+// Iterative, recursion, memoization
+
+// Solution #3, memoization solution (improved recursion) O(n)
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+console.log(slowFib(process.argv[2]));
+
+// // Solution #1, iterative solution O(n)
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
+
+// // Solution #2, recursive solution O(2^n)
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// // Solution #1, iterative solution
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
