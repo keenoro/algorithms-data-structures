7176bad08e026ddb608cedaa763d6c8f99edc098
keenoro <kkhoo93@gmail.com>
2020-05-15 16:00:21 +1000

    Created tree data structure

---
+++ data_structures/tree.js
@@ -0,0 +1,47 @@
+// TREE
+// Tree and node classes
+// Node contains the data and children
+// Tree has root and traversal methods
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => {
+      return node.data !== data;
+    });
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  // Breath-First Traversal
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  // Depth-First Traversal
+  traverseDF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
